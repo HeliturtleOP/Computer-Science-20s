890936b2c785ba73e121f5c95e526ac7a59ff07d
HeliturtleOP <47545028+HeliturtleOP@users.noreply.github.com>
2021-03-06 23:42:37 -0600

    Delete VBP1-1 directory

--- VBP1-1/VBP1-1/Form1.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form1
-    Private Sub Label1_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        End
-    End Sub
-End Class
